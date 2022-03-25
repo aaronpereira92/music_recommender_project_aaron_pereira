c1d46599165cd58f7182c6d214eec432c74f6e71
aaronpereira92 <aaron.pereira92@gmail.com>
2022-03-25 12:41:05 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Welcome to the main Notebook made by Aaron Pereira for the GNOD project. In this notebook, I will demonstrate how the **song recommender** works, which is a function that I made in the previous lab. The function already imports all the necessary libraries for it to work. You can check that list of libraries in the _libraries folder, there is a txt file
+
+# If you want to see more information on how the function is made, please have a look at the notebook: **lab_kmeans_clustering.ipynb**
+
+#  
+
+# **VERY IMPORTANT** : You need the config file for your SpotifyAPI credentials login. The function will only work if this file is present. Thanks
+
+# **1.** I'm importing the song_recommender from the **functions.py** file present in the functions folder. 
+
+# In[1]:
+
+
+from functions import song_recommender
+
+
+# In[2]:
+
+
+song_recommender()
+
+
+# In[ ]:
+
+
+
+
